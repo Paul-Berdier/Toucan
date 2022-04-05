89fcf8b82c92a85b20bb03ac16812b7a20b482d8
Paul-Berdier <paul.berdier@ynov.com>
2022-04-05 11:36:45 +0200

    init:db(like)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_24_001257) do
+ActiveRecord::Schema.define(version: 2022_04_05_093455) do
+
+  create_table "likes", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "toucs", force: :cascade do |t|
     t.text "touc"
