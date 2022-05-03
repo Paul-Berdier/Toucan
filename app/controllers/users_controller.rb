777d6cf3e64351df3d47a0854d99b7bf5c6b4569
Damien Esp <d.espene@gmail.com>
2022-05-03 14:30:21 +0200

    NoMethodError 'user_path' fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.fing(params[:id])
-    @users = User.all_execpt(current_user)
+    @users = User.all_except(current_user)
   end
 end
