39f215ba4710309554d245569cd150707d346d4e
Paul-Berdier <paul.berdier@ynov.com>
2022-05-05 05:46:35 +0200

    init:like feed files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :toucs
+  resources :toucs do
+    resources :likes
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "toucs#index"
 end
